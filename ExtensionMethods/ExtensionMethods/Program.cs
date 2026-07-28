7190b3a08d5116e5847b380ca848f0e2460426da
Mohd Zaid <mohdzaidrmp786@gmail.com>
2026-07-28 14:47:08 +0530

    Add ExtensionMethods project

---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very long blog post blah blah blah...";
+            var shortenedPost = post.Shorten(5);
+
+            Console.WriteLine(shortenedPost);
+
+            Console.WriteLine();
+
+            // using extensions methods instead of creating them
+            IEnumerable<int> numbers = new List<int>() { 1, 5, 2, 7, 10, 4, 18 };
+            var max = numbers.Max();
+
+            Console.WriteLine(max);
+        }
+    }
+}
